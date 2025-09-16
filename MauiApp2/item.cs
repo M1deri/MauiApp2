421359dce3d4eebccadffba17caf53618afde78b
M1deri <112771150+M1deri@users.noreply.github.com>
2025-09-16 12:08:34 +0200

    Add files via upload

---
+++ MauiApp2/item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp2
+{
+    internal class Item
+    {
+        public string? Name { get; set; }
+        public string? Quantity { get; set; }
+    }
+}
